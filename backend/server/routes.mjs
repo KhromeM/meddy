6cc5ceff27578392d5ec84fcc8ec250f0cc67e7d
KhromeM <khrome@nyu.edu>
2024-06-13 08:48:26 -0700

    server testing

--- backend/server/routes.mjs
+++ backend/server/routes.mjs
@@ -28,6 +28,17 @@ app.use(json());
 // });
 
 // HTTP endpoint for chat
+app.get("/", async (req, res) => {
+	try {
+		console.log("Hello user!");
+		res.json({ status: "success" });
+	} catch (err) {
+		console.error(err);
+		res.json({ status: "fail", message: "Failed to reach Gemini" });
+	}
+	res.end();
+});
+
 app.post("/chat", async (req, res) => {
 	try {
 		const text = req.body.message.text;
