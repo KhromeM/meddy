69b514c44da99efaf1361a5e29e9ab74c17d5602
KhromeM <khrome@nyu.edu>
2024-07-04 21:46:34 -0700

    fixed error handling for SSE

--- backend/ai/langAi/chatStream.mjs
+++ backend/ai/langAi/chatStream.mjs
@@ -15,7 +15,7 @@ import CONFIG from "../../config.mjs";
 import { createDefaultSystemPrompt } from "../prompts/default.mjs";
 let defaultModel = CONFIG.TEST
 	? groqModel // use groq for tests, other models may timeout
-	: openAIModel || anthropicModel || vertexAIModel || openAIModel;
+	: vertexAIModel || anthropicModel || vertexAIModel || openAIModel;
 
 const systemPrompts = { 0: createDefaultSystemPrompt }; // globals or imports
 const fewShotExamples = {}; // globals or imports
