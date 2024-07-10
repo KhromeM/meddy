2cc01e27a327d182c134230a0c328fe6822eeb7b
Shashank datta Bezgam <shashankdatta@vt.edu>
2024-07-10 17:03:58 -0400

    Completed Grog Speech-To-Text API Integration

--- backend/ai/audio/speechToText.mjs
+++ backend/ai/audio/speechToText.mjs
@@ -1,54 +1,43 @@
-import fs from "fs";
 import Groq from "groq-sdk";
-import WebSocket from "ws";
 
 const model = "whisper-large-v3";
-
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
-async function transcribeSpeech(audioFilePath, language = "en") {
-  let audioFileStream = fs.createReadStream(audioFilePath);
-
-  const transcription = await groq.audio.transcriptions.create({
-    file: audioFileStream,
-    model: model,
-    prompt: "Specify context or spelling",
-    response_format: "json",
-    language: language, // Optional
-    temperature: 0.0,
-  });
-  console.log(transcription.text);
-}
-
-async function translateSpeech(audioFilePath) {
-  let audioFileStream = fs.createReadStream(audioFilePath);
-
-  const translation = await groq.audio.translations.create({
-    file: audioFileStream,
-    model: model,
-    prompt: "Specify context or spelling",
-    response_format: "json",
-    temperature: 0.0,
-  });
-  console.log(translation.text);
-}
-
-async function speechToText() {
-  const wss = new WebSocket.Server({ port: 8080 });
-
-  wss.on("connection", (ws) => {
-    let transcription = "";
-
-    ws.on("message", async (message) => {
-      if (message instanceof Buffer) {
-        const audioChunk = Buffer.from(message, "base64");
-        const audioFileStream = fs.createReadStream(audioChunk);
-
-        const transcription = await transcribeSpeech(audioFileStream);
-        transcription += transcription;
-      } else {
-        ws.send(transcription);
-      }
+// Speech to text with optional language parameter (default is English)
+// Optional language parameter might result in faster transcription
+// Language codes: https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes
+async function transcribeSpeech(stream, tscLanguage = "en") {
+  try {
+    const transcription = await groq.audio.transcriptions.create({
+      file: stream,
+      model: model,
+      prompt: "Specify context or spelling",
+      response_format: "json",
+      language: tscLanguage,
+      temperature: 0.0,
     });
-  });
+
+    return transcription.text;
+  } catch (error) {
+    console.error("Error transcribing speech:", error);
+    throw error;
+  }
+}
+
+// Always translates to only English by default
+async function translateSpeech(stream) {
+  try {
+    const translation = await groq.audio.translations.create({
+      file: stream,
+      model: model,
+      prompt: "Specify context or spelling",
+      response_format: "json",
+      temperature: 0.0,
+    });
+
+    return translation.text;
+  } catch (error) {
+    console.error("Error translating speech:", error);
+    throw error;
+  }
 }
