e88a6d058a35df44da8685b0c221ba2ca31ef24e
iamudyavar <aditya.udyavar@gmail.com>
2024-07-19 13:22:16 -0500

    Added try-catch

--- backend/server/controllers/medplumController.mjs
+++ backend/server/controllers/medplumController.mjs
@@ -3,7 +3,12 @@ const medplum = new MedplumClient();
 await medplum.startClientLogin(process.env.MEDPLUM_CLIENT_ID, process.env.MEDPLUM_CLIENT_SECRET);
 
 export const getPatientDetails = async (req, res) => {
-	const patientId = req.params.patientId;
-	const patientInfo = await medplum.readPatientEverything(patientId);
-	res.status(200).json(patientInfo);
+	try {
+		const patientId = req.params.patientId;
+		const patientInfo = await medplum.readPatientEverything(patientId);
+		res.status(200).json(patientInfo);
+	} catch (err) {
+		console.error("Error fetching patient details from Medplum:", err);
+		res.status(500).json({ status: "fail", message: "Failed to fetch patient details from Medplum" });
+	}
 };
