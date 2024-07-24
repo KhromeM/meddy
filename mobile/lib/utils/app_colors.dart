8a6e250659065a8afa62e37e27648dd2703746a1
Siv <var.mironov@gmail.com>
2024-07-24 14:18:09 -0300

    set up file for theme colors

---
+++ mobile/lib/utils/app_colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color lightPurple = Color(0xFFD1BDFC); 
+const Color lightGreen = Color(0xFFDAFFCB); 
+const Color orangeAccent = Color(0xFFFFA73F); 
