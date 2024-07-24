2805efc6d5b91317f9a61f6eddbd911f7303ff38
Za Mustafa <68210337+KhromeM@users.noreply.github.com>
2024-07-24 14:43:56 -0400

    Revert "Refined stop gen button"

--- mobile/lib/widgets/animated_stop_button.dart
+++ mobile/lib/widgets/animated_stop_button.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:jumping_dot/jumping_dot.dart';
 
 class AnimatedStopButton extends StatefulWidget {
   final VoidCallback onPressed;
@@ -11,9 +10,32 @@ class AnimatedStopButton extends StatefulWidget {
 
 class _AnimatedStopButtonState extends State<AnimatedStopButton>
     with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  final List<Animation<double>> _dotAnimations = [];
+
   @override
   void initState() {
     super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1000),
+    )..repeat(reverse: true);
+    for (int i = 0; i < 3; i++) {
+      _dotAnimations.add(
+        Tween<double>(begin: 0.0, end: -5.0).animate(
+          CurvedAnimation(
+            parent: _controller,
+            curve: Interval(i * 0.2, (i + 1) * 0.2, curve: Curves.easeInOut),
+          ),
+        ),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -23,15 +45,35 @@ class _AnimatedStopButtonState extends State<AnimatedStopButton>
       child: Row(
         mainAxisSize: MainAxisSize.min,
         children: [
-          JumpingDots(
-            color: Colors.black,
-            radius: 10,
-            numberOfDots: 3,
-          ),
+          _buildDots(),
           SizedBox(width: 8),
           _buildButton(),
         ],
       ),
+    );
+  }
+
+  Widget _buildDots() {
+    return Row(
+      children: List.generate(3, (index) {
+        return AnimatedBuilder(
+          animation: _dotAnimations[index],
+          builder: (context, child) {
+            return Transform.translate(
+              offset: Offset(0, _dotAnimations[index].value),
+              child: Container(
+                width: 6,
+                height: 6,
+                margin: EdgeInsets.symmetric(horizontal: 2),
+                decoration: BoxDecoration(
+                  color: Colors.grey[600],
+                  shape: BoxShape.circle,
+                ),
+              ),
+            );
+          },
+        );
+      }),
     );
   }
 
@@ -49,19 +91,18 @@ class _AnimatedStopButtonState extends State<AnimatedStopButton>
         mainAxisSize: MainAxisSize.min,
         children: [
           Container(
-            width: 20,
-            height: 20,
+            width: 24,
+            height: 24,
             decoration: BoxDecoration(
               shape: BoxShape.circle,
               color: Colors.black,
             ),
-            child: Icon(Icons.circle, size: 15, color: Colors.orange),
+            child: Icon(Icons.stop, size: 16, color: Colors.orange),
           ),
           SizedBox(width: 8),
           Text(
             'Stop generating',
-            style: TextStyle(
-                color: Colors.black, fontWeight: FontWeight.w700, fontSize: 12),
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
           ),
         ],
       ),
