6419f4bfa8ab63693f749640855ae9173b666e71
Siv <var.mironov@gmail.com>
2024-07-24 14:19:33 -0300

    made bottom sheet look more like figma

--- mobile/lib/widgets/custom_app_bar.dart
+++ mobile/lib/widgets/custom_app_bar.dart
@@ -1,9 +1,9 @@
 // appbar for main page
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:meddymobile/pages/chat_page.dart';
 import 'package:meddymobile/pages/profile_page.dart';
+import 'package:meddymobile/utils/app_colors.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final double appBarHeight = 56.0;
@@ -13,28 +13,29 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
       context: context,
       backgroundColor: Colors.white, 
       builder: (context)=> SizedBox(
+        //heightFactor: 0.8,
         width: MediaQuery.of(context).size.width,
-        height: 500,
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              ElevatedButton(
-                onPressed: () { 
-                  Navigator.push(
+              _buildButton(
+              'Reminders',
+              () {
+                Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => const ProfilePage()),
-              );
-            },
-            child: Text('Reminders'),
-          ),
-              ElevatedButton(
-              onPressed: () {
-                //TODO: make health/research page
+                );
               },
-            child: Text('Your health'),
-          ),
+            ),
+            SizedBox(height: 20),
+              _buildButton(
+              'Your Health',
+              () {
+                //TODO: nav to health page
+              },
+            )
           // You can add more widgets here if needed
         ],
       ),
@@ -42,7 +43,32 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
     ));
   }
 
-
+Widget _buildButton(String text, VoidCallback onPressed) {
+  return SizedBox(
+    width: double.infinity, 
+    child: ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: lightPurple,
+        foregroundColor: Colors.black,
+        padding: EdgeInsets.symmetric(vertical: 20),
+        alignment: Alignment.centerLeft, 
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        elevation: 0, 
+      ),
+      child: Padding(
+        padding: EdgeInsets.only(left: 16),
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 24,
+          fontWeight: FontWeight.w600),
+      ),
+      ),
+    ),
+  );
+}
   
   @override
   Widget build(BuildContext context) {
@@ -52,10 +78,6 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
       leading: InkWell(
         onTap: () {
           _showBottomSheet(context);
-          // Navigator.push(
-          //   context,
-          //   MaterialPageRoute(builder: (context) => const ProfilePage()),
-          // );
         },
         child: Padding(
           padding: const EdgeInsets.only(left: 10),
