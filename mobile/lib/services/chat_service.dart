6eacb97586098e5912527468c5828b8abb8a7c60
blin4504 <bl4504@g.rit.edu>
2024-07-26 17:32:45 -0400

    feat: image upload feature but buggy

--- mobile/lib/services/chat_service.dart
+++ mobile/lib/services/chat_service.dart
@@ -1,10 +1,15 @@
 import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
 import 'package:http/http.dart' as http;
 import 'package:meddymobile/models/message.dart';
+import 'package:path/path.dart' as path;
+
 
 class ChatService {
-  static const String baseUrl = //'http://trymeddy.com/api';
-      'http://localhost:8000/api';
+  static const String baseUrl =
+      'https://trymeddy.com/api';
+      // 'http://localhost:8000/api';
 
   Future<List<Message>> getChatHistory() async {
     try {
@@ -12,15 +17,63 @@ class ChatService {
         "idToken": "dev",
       });
       List<dynamic> chatHistory = jsonDecode(response.body)['chatHistory'];
-      print("Parsing messages..");
       var messages =
           chatHistory.map((message) => Message.fromJson(message)).toList();
-      print("MESSAGES: ");
-      print(messages);
       return messages;
     } catch (e) {
       print(e.toString());
       return List.empty();
+    }
+  }
+
+  Future<void> uploadImage(String image) async {
+    final bytes = await File(image).readAsBytes();
+    final base64Image = base64Encode(bytes);
+    final response = await http.post(
+      Uri.parse('$baseUrl/image'),
+      headers: {
+        'Content-Type': 'application/json',
+        'idToken': 'dev',
+      },
+      body: jsonEncode({
+        'image': {
+          'name': path.basename(image),
+          'data': base64Image,
+        },
+      }),
+    );
+    print("Uploading status ${response.statusCode}");
+    if (response.statusCode == 200) {
+      final responseData = jsonDecode(response.body);
+      print("Uploaded $responseData");
+    } else {
+      print('Error uploading image: ${response.body}');
+    }
+  }
+
+  Future<Uint8List?> fetchImage(String imageId) async {
+    try {
+      final uri = Uri.parse('$baseUrl/image').replace(queryParameters: {
+        'image': imageId,
+      });
+
+      final response = await http.get(
+        uri,
+        headers: {
+          'Content-Type': 'application/json',
+          'idToken': 'dev',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        return response.bodyBytes;
+      } else {
+        print('Error fetching image: ${response.statusCode}');
+        return null;
+      }
+    } catch (e) {
+      print('Failed to fetch image: $e');
+      return null;
     }
   }
 }
