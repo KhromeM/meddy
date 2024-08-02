3522aaf05b5f7725aacdc60f58b0a6d2b4679959
YaBoiAli <alinawab9830@gmail.com>
2024-08-02 21:49:06 +0400

    removed unused file

--- web-frontend/src/App.jsx
+++ web-frontend/src/App.jsx
@@ -7,7 +7,6 @@ import "./styles/chat.css";
 import { AuthProvider } from "./firebase/AuthService.jsx";
 import customTheme from "./theme";
 import { EVI } from "./components/EVI.jsx";
-import { Team } from "./components/Team.jsx";
 import { Contact } from "./components/contact.jsx";
 import { AboutUsPage } from "./components/about.jsx";
 
@@ -20,7 +19,6 @@ function App() {
             <Route path="/" element={<LandingPage />} />
             <Route path="/chat" element={<Chat />} />
             <Route path="/EVI" element={<EVI />} />
-            <Route path="/team" element={<Team />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/about" element={<AboutUsPage />} />
           </Routes>
