2fa7258b027dade09a993e1d6e23537cb147cee6
KhromeM <khrome@nyu.edu>
2024-08-03 13:18:09 -0400

    small fix

--- web-frontend/src/server/imageHandler.js
+++ web-frontend/src/server/imageHandler.js
@@ -1,72 +1,73 @@
 import axios from "axios";
+import { serverUrl } from "../utils/Info";
 
-const baseUrl = "http://localhost:8000/api";
+const baseUrl = serverUrl.http;
 const readFileAsBase64 = (file) => {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
+	return new Promise((resolve, reject) => {
+		const reader = new FileReader();
 
-    reader.onloadend = () => {
-      resolve(reader.result.split(",")[1]);
-    };
+		reader.onloadend = () => {
+			resolve(reader.result.split(",")[1]);
+		};
 
-    reader.onerror = (error) => {
-      reject(error);
-    };
+		reader.onerror = (error) => {
+			reject(error);
+		};
 
-    reader.readAsDataURL(file);
-  });
+		reader.readAsDataURL(file);
+	});
 };
 export const uploadImage = async (file, user) => {
-  const token = user?.stsTokenManager?.accessToken;
-  const userid = user?.uid;
-  const base64data = await readFileAsBase64(file);
-  const payload = {
-    image: {
-      name: file.name,
-      data: base64data,
-    },
-    idToken: token,
-  };
-  try {
-    const response = await axios.post(
-      `${baseUrl}/image?userId=${userid}`,
-      payload,
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-    return response;
-  } catch (error) {
-    console.error(
-      "Error uploading image:",
-      error.response?.data?.message || error.message
-    );
-  }
+	const token = user?.stsTokenManager?.accessToken;
+	const userid = user?.uid;
+	const base64data = await readFileAsBase64(file);
+	const payload = {
+		image: {
+			name: file.name,
+			data: base64data,
+		},
+		idToken: token,
+	};
+	try {
+		const response = await axios.post(
+			`${baseUrl}/image?userId=${userid}`,
+			payload,
+			{
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${token}`,
+				},
+			}
+		);
+		return response;
+	} catch (error) {
+		console.error(
+			"Error uploading image:",
+			error.response?.data?.message || error.message
+		);
+	}
 };
 
 export const getImage = (file, user) => {
-  const token = user?.stsTokenManager?.accessToken;
-  const userid = user?.uid;
-  const fileName = file?.name;
+	const token = user?.stsTokenManager?.accessToken;
+	const userid = user?.uid;
+	const fileName = file?.name;
 
-  try {
-    const response = axios.get(
-      `${baseUrl}/image/base64?image=${fileName}&userId=${userid}`,
-      {
-        headers: {
-          "Content-Type": "application/json",
-          idToken: token,
-        },
-      }
-    );
-    return response;
-  } catch (error) {
-    console.error(
-      "Error retriving image:",
-      error.response?.data?.message || error.message
-    );
-  }
+	try {
+		const response = axios.get(
+			`${baseUrl}/image/base64?image=${fileName}&userId=${userid}`,
+			{
+				headers: {
+					"Content-Type": "application/json",
+					idToken: token,
+				},
+			}
+		);
+		return response;
+	} catch (error) {
+		console.error(
+			"Error retriving image:",
+			error.response?.data?.message || error.message
+		);
+	}
 };
