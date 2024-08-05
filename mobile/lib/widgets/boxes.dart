cd795131e447f5cc67bbe4407356b3516459694d
Siv <var.mironov@gmail.com>
2024-08-04 23:50:44 -0300

    chore: made home poxes glassy

--- mobile/lib/widgets/boxes.dart
+++ mobile/lib/widgets/boxes.dart
@@ -4,18 +4,20 @@ import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:meddymobile/pages/chat_page.dart';
 import 'package:meddymobile/utils/languages.dart';
 import 'package:provider/provider.dart';
+import 'dart:ui';
+
 class Boxes extends StatelessWidget {
   final List<String> texts;
   final List<String>? images;
   final bool isHighContrast;
 
   static const List<Color> pastelColors = [
-    Color(0xFFFFD1DC), // Pastel pink
-    Color(0xFFFFE5B4), // Pastel yellow
-    Color(0xFFD0F0C0), // Pastel green
-    Color(0xFFB0E0E6), // Pastel blue
+    Color.fromARGB(255, 248, 220, 227), // Pastel pink
+    Color.fromARGB(255, 254, 237, 205), // Pastel yellow
+    Color.fromARGB(255, 225, 247, 213), // Pastel green
+    Color.fromARGB(255, 214, 246, 250), // Pastel blue
     Color(0xFFF0E6FF), // Pastel purple
-    Color(0xFFFFDAB9), // Pastel orange
+    Color.fromARGB(255, 249, 229, 211), // Pastel orange
   ];
 
   static const List<Color> highContrastColors = [
@@ -33,80 +35,66 @@ class Boxes extends StatelessWidget {
     this.images,
     required this.isHighContrast,
   });
-  void onBoxTap(
-      BuildContext context, final String myPrompt) {
+
+  void onBoxTap(BuildContext context, final String myPrompt) {
     Navigator.push(
       context,
       MaterialPageRoute(
-          builder: (context) =>
-              ChatPage(initialPrompt: myPrompt)),
+        builder: (context) => ChatPage(initialPrompt: myPrompt),
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Consumer<LanguageProvider>(
-    builder: (context, languageProvider, child) {
-    return Expanded(
-      child: MasonryGridView.count(
-        scrollDirection: Axis.horizontal,
-        padding: EdgeInsets.all(10.0),
-        crossAxisCount: 2,
-        mainAxisSpacing: 10.0,
-        crossAxisSpacing: 10.0,
-        itemCount: texts.length,
-        itemBuilder: (context, index) {
-          return GestureDetector(
-              onTap: () => onBoxTap(context, languageProvider.translate(texts[index])),
-              child: Container(
-                width: index.isEven ? 200 : 170,
-                height: Random().nextInt(100) + 50,
-                decoration: BoxDecoration(
-                  color: isHighContrast
-                      ? highContrastColors[
-                          index % pastelColors.length]
-                      : pastelColors[
-                          index % pastelColors.length],
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Stack(
-                  children: [
-                    if (images != null &&
-                        index < images!.length)
-                      Positioned.fill(
-                        child: ClipRRect(
-                          borderRadius:
-                              BorderRadius.circular(20),
-                          child: Image.asset(
-                            images![index],
-                            fit: BoxFit.cover,
-                          ),
+      builder: (context, languageProvider, child) {
+        return Expanded(
+          child: MasonryGridView.count(
+            scrollDirection: Axis.horizontal,
+            padding: EdgeInsets.all(10.0),
+            crossAxisCount: 2,
+            mainAxisSpacing: 10.0,
+            crossAxisSpacing: 10.0,
+            itemCount: texts.length,
+            itemBuilder: (context, index) {
+              Color boxColor = isHighContrast
+                  ? highContrastColors[index % highContrastColors.length]
+                  : pastelColors[index % pastelColors.length];
+              return GestureDetector(
+                onTap: () => onBoxTap(context, languageProvider.translate(texts[index])),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                    child: Container(
+                      width: index.isEven ? 200 : 170,
+                      height: Random().nextInt(100) + 50,
+                      decoration: BoxDecoration(
+                        color: boxColor.withOpacity(0.7),
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          color: Colors.black.withOpacity(0.1),
+                          width: 1,
                         ),
                       ),
-                    Positioned(
-                      top: 0,
-                      left: 0,
-                      right: 0,
                       child: Padding(
-                        padding: EdgeInsets.all(12.0),
+                        padding: const EdgeInsets.all(16.0),
                         child: Text(
                           languageProvider.translate(texts[index]),
-                          style: TextStyle(
-                            color: const Color.fromARGB(
-                                255, 0, 0, 0),
-                            fontSize: 20,
-                            fontWeight: FontWeight.w500,
-                          ),
-                          maxLines: 4,
-                          // overflow: TextOverflow.ellipsis,
+                          style: TextStyle(color: Colors.black,
+                          fontSize: 18),
+                          
                         ),
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ));
-        },
-      ),
-    );},);
+              );
+            },
+          ),
+        );
+      },
+    );
   }
 }
