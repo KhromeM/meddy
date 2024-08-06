8eb2ca1dafd72aa65d9b274d5ebfd8b2f41499f8
Duy Doan <ddoan14@ivytech.edu>
2024-08-05 18:32:59 -0700

    fix home page

--- mobile/lib/pages/my_home_page.dart
+++ mobile/lib/pages/my_home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:meddymobile/services/auth_service.dart';
 import 'package:provider/provider.dart';
 import 'package:meddymobile/providers/chat_provider.dart';
 import 'package:meddymobile/utils/app_colors.dart';
@@ -34,6 +36,9 @@ class _MyHomePageState extends State<MyHomePage> {
   late PlayerService playerService;
   late RecorderService recorderService;
 
+  final AuthService _authService = AuthService();
+  late String _firstName;
+
   @override
   void initState() {
     super.initState();
@@ -42,6 +47,8 @@ class _MyHomePageState extends State<MyHomePage> {
     wsConnection = WSConnection();
     playerService = PlayerService(wsConnection);
     recorderService = RecorderService(wsConnection);
+
+    _firstName = _authService.getFirstName() ?? 'User';
 
     // Connect to WebSocket
     wsConnection.connect();
@@ -70,13 +77,38 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Stack(
       children: [
-        MainBackground(),
+        //MainBackground(),
         Scaffold(
-          backgroundColor: Colors.transparent,
+          extendBodyBehindAppBar: true,
+          backgroundColor: Colors.white,
           appBar: CustomAppBar(),
           body: Column(
             children: [
-              SizedBox(height: 20),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.07),
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Row(
+                  children: [
+                    Text(
+                      'Hey!',
+                      style: TextStyle(fontSize: 40),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Row(
+                  children: [
+                    Text(
+                      _firstName,
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.02),
               InkWell(
                 onTap: () {
                   _showMic();
@@ -85,17 +117,69 @@ class _MyHomePageState extends State<MyHomePage> {
                   alignment: Alignment.center,
                   children: [
                     Container(
-                      width: 100.0,
-                      height: 100.0,
+                      width: MediaQuery.of(context).size.width * 0.95,
+                      height: 100,
                       decoration: BoxDecoration(
-                        color: orangeAccent,
-                        shape: BoxShape.circle,
-                      ),
+                          color: Color.fromRGBO(1, 99, 218, 1),
+                          shape: BoxShape.rectangle,
+                          borderRadius: BorderRadius.circular(20)),
                     ),
-                    Icon(
-                      Icons.mic,
-                      size: 50,
-                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Column(
+                          children: [
+                            Stack(
+                              alignment: Alignment.center,
+                              children: [
+                                Icon(
+                                  Icons.circle,
+                                  size: 75,
+                                  color: Colors.white,
+                                ),
+                                Icon(Icons.phone_in_talk,
+                                    color: Color.fromRGBO(1, 99, 218, 1),
+                                    size: 30),
+                              ],
+                            )
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Start Voice Call',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                            Text(
+                              'or Translate my doctor',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w300),
+                            ),
+                            Text(
+                              'or Listen in with me to the doctor',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w300),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.white,
+                        ),
+                        SizedBox(width: 10)
+                      ],
+                    )
                   ],
                 ),
               ),
@@ -104,10 +188,48 @@ class _MyHomePageState extends State<MyHomePage> {
                 texts: texts,
                 isHighContrast: highContrastMode?.isHighContrast ?? false,
               ),
-              SizedBox(height: 100),
+              SizedBox(height: 15),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+                Text(
+                  'Integrated with your favorite apps',
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w800,
+                      color: const Color.fromARGB(255, 71, 71, 71)),
+                )
+              ]),
+              SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Color.fromRGBO(254, 249, 239, 1),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(20),
+                      child:
+                          Center(child: Image.asset('assets/images/epic.png')),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Color.fromRGBO(254, 249, 239, 1),
+                    ),
+                    child: Center(
+                        child: Image.asset('assets/images/googlefit.png')),
+                  ),
+                ],
+              )
             ],
           ),
-
         ),
       ],
     );
--- mobile/lib/widgets/boxes.dart
+++ mobile/lib/widgets/boxes.dart
@@ -2,14 +2,24 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:meddymobile/pages/chat_page.dart';
+import 'package:meddymobile/utils/app_colors.dart';
 import 'package:meddymobile/utils/languages.dart';
 import 'package:provider/provider.dart';
 import 'dart:ui';
 
 class Boxes extends StatelessWidget {
   final List<String> texts;
-  final List<String>? images;
   final bool isHighContrast;
+
+  // Added some random image links for demonstration
+  final List<String> images = [
+    'https://picsum.photos/200',
+    'https://picsum.photos/201',
+    'https://picsum.photos/202',
+    'https://picsum.photos/203',
+    'https://picsum.photos/204',
+    'https://picsum.photos/205',
+  ];
 
   static const List<Color> pastelColors = [
     Color.fromARGB(255, 248, 220, 227), // Pastel pink
@@ -29,10 +39,9 @@ class Boxes extends StatelessWidget {
     Color(0xFFFFFFFF), // White
   ];
 
-  const Boxes({
+  Boxes({
     super.key,
     required this.texts,
-    this.images,
     required this.isHighContrast,
   });
 
@@ -49,27 +58,29 @@ class Boxes extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<LanguageProvider>(
       builder: (context, languageProvider, child) {
-        return Expanded(
+        return Container(
+          height: MediaQuery.of(context).size.height * 0.3,
           child: MasonryGridView.count(
             scrollDirection: Axis.horizontal,
-            padding: EdgeInsets.all(10.0),
-            crossAxisCount: 2,
-            mainAxisSpacing: 10.0,
-            crossAxisSpacing: 10.0,
+            padding: EdgeInsets.symmetric(
+                horizontal: MediaQuery.of(context).size.width * 0.03),
+            crossAxisCount: 1,
+            mainAxisSpacing: MediaQuery.of(context).size.width * 0.03,
+            crossAxisSpacing: MediaQuery.of(context).size.width * 0.03,
             itemCount: texts.length,
             itemBuilder: (context, index) {
               Color boxColor = isHighContrast
                   ? highContrastColors[index % highContrastColors.length]
                   : pastelColors[index % pastelColors.length];
               return GestureDetector(
-                onTap: () => onBoxTap(context, languageProvider.translate(texts[index])),
+                onTap: () =>
+                    onBoxTap(context, languageProvider.translate(texts[index])),
                 child: ClipRRect(
-                  borderRadius: BorderRadius.circular(12),
+                  borderRadius: BorderRadius.circular(20),
                   child: BackdropFilter(
                     filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
                     child: Container(
-                      width: index.isEven ? 200 : 170,
-                      height: Random().nextInt(100) + 50,
+                      width: MediaQuery.of(context).size.width * 0.45,
                       decoration: BoxDecoration(
                         color: boxColor.withOpacity(0.7),
                         borderRadius: BorderRadius.circular(12),
@@ -78,14 +89,34 @@ class Boxes extends StatelessWidget {
                           width: 1,
                         ),
                       ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Text(
-                          languageProvider.translate(texts[index]),
-                          style: TextStyle(color: Colors.black,
-                          fontSize: 18),
-                          
-                        ),
+                      child: Column(
+                        children: [
+                          Container(
+                            width: double.infinity,
+                            height: MediaQuery.of(context).size.height * 0.15,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image:
+                                    NetworkImage(images[index % images.length]),
+                                fit: BoxFit.cover,
+                              ),
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(20),
+                                topRight: Radius.circular(20),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 8.0),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                top: 10, right: 10, left: 10),
+                            child: Text(
+                              languageProvider.translate(texts[index]),
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 18),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
