65618c9e7585d550e95aca393204705ad6ceb0fa
Siv <var.mironov@gmail.com>
2024-08-06 23:14:33 -0300

    updated navbar

--- mobile/lib/pages/screen_wrapper.dart
+++ mobile/lib/pages/screen_wrapper.dart
@@ -5,51 +5,8 @@ import 'package:meddymobile/pages/my_home_page.dart';
 import 'package:meddymobile/pages/reminder_page.dart';
 import 'package:meddymobile/widgets/bottom_bar.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-
-final _bottomNavigationBarItems = [
-  const BottomNavigationBarItem(
-    icon: Icon(
-      Icons.home,
-    ),
-    activeIcon: Icon(
-      Icons.home,
-      color: Color(0xFFD20062),
-    ),
-    label: '',
-    backgroundColor: null,
-  ),
-  const BottomNavigationBarItem(
-    icon: Icon(
-      Icons.chat,
-    ),
-    activeIcon: Icon(
-      Icons.chat,
-      color: Color(0xFFD895DA),
-    ),
-    label: '',
-  ),
-  const BottomNavigationBarItem(
-    icon: Icon(
-      Icons.monitor_heart,
-    ),
-    activeIcon: Icon(
-      Icons.monitor_heart,
-      color: Colors.orange,
-    ),
-    label: '',
-  ),
-  const BottomNavigationBarItem(
-    icon: Icon(
-      Icons.event,
-    ),
-    activeIcon: Icon(
-      Icons.event,
-      color: Colors.purple,
-    ),
-    label: '',
-  ),
-];
-
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:meddymobile/utils/app_colors.dart';
 class ScreenWrapper extends StatefulWidget {
   const ScreenWrapper({super.key});
 
@@ -59,8 +16,35 @@ class ScreenWrapper extends StatefulWidget {
 
 class _ScreenWrapperState extends State<ScreenWrapper> {
   int _currentIndex = 0;
-
   final PageController _pageController = PageController(initialPage: 0);
+
+  List<BottomNavigationBarItem> get _bottomNavigationBarItems {
+    return [
+      _buildNavigationItem('assets/icons/Home_light.svg', 0),
+      _buildNavigationItem('assets/icons/comment_light.svg', 1),
+      _buildNavigationItem('assets/icons/stethoscope_light.svg', 2),
+      _buildNavigationItem('assets/icons/Date_range_light.svg', 3),
+    ];
+  }
+
+  BottomNavigationBarItem _buildNavigationItem(String assetName, int index) {
+    Color color = _currentIndex == index ? Colors.brown : Colors.grey;
+    return BottomNavigationBarItem(
+      icon: SvgPicture.asset(
+        assetName,
+        height: 40,
+        width: 40,
+        colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      ),
+      activeIcon: SvgPicture.asset(
+        assetName,
+        height: 40,
+        width: 40,
+        colorFilter: ColorFilter.mode(Colors.brown, BlendMode.srcIn),
+      ),
+      label: '',
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
