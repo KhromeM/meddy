bcae946e2fe01300ceacf99d18ac82b5cd3140a4
TheForeverOptimist <sean@elevate.app>
2024-08-07 23:00:23 -0700

    updated audio user texts

--- web-frontend/src/components/MessageList.jsx
+++ web-frontend/src/components/MessageList.jsx
@@ -2,16 +2,27 @@ import React from "react";
 import { VStack } from "@chakra-ui/react";
 import Message from "./Message";
 
-const MessageList = ({ messages, messagesEndRef, inProgress }) => {
+const MessageList = ({ messages, messagesEndRef, inProgress, currentTranscription }) => {
   return (
     <VStack spacing={4} align="stretch">
       {messages.map((msg, index) => (
         <Message
           key={msg.messageid}
           message={msg}
-          isStreaming={index === messages.length - 1 && inProgress}
+          isStreaming={index === messages.length - 1 && inProgress && !currentTranscription}
         />
       ))}
+      {currentTranscription && (
+        <Message 
+          message={{
+            source: "user",
+            text: currentTranscription.text,
+            time: new Date(),
+            isComplete: false,
+          }}
+        isStreaming={true}
+        />
+      )}
       <div ref={messagesEndRef} />
     </VStack>
   );
