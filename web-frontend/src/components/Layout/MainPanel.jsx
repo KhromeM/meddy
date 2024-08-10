e8b926bb28b8a975a758259e2674753f5bee5876
WILLIAM CHAN <wc2184@nyu.edu>
2024-08-09 21:33:26 -0400

    change health page, remove padding top on main content

--- web-frontend/src/components/Layout/MainPanel.jsx
+++ web-frontend/src/components/Layout/MainPanel.jsx
@@ -1,18 +1,39 @@
 import { Box, useStyleConfig } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { Gradient } from "../Gradient";
 function MainPanel(props) {
-	const { variant, children, ...rest } = props;
-	const styles = useStyleConfig("MainPanel", { variant });
-	// Pass the computed styles into the `__css` prop
-	return (
-		<Box
-			__css={styles}
-			br={10}
-			backgroundColor={props.backgroundColor}
-			{...rest}
-		>
-			{children}
-		</Box>
-	);
+  const { variant, children, ...rest } = props;
+  const styles = useStyleConfig("MainPanel", { variant });
+  // Pass the computed styles into the `__css` prop
+
+  //   useEffect(() => {
+  //     const gradient = new Gradient();
+  //     gradient.initGradient("#gradient-canvas");
+  //   }, []);
+  return (
+    <Box
+      __css={styles}
+      br={10}
+      backgroundColor={props.backgroundColor}
+      minHeight="100%"
+      {...rest}
+    >
+      {/* <canvas
+        id="gradient-canvas"
+        data-js-darken-top
+        data-transition-in
+        style={{
+          position: "absolute",
+          width: "100%",
+          minHeight: "100%",
+          top: 0,
+          left: 0,
+          zIndex: -1,
+        }}
+      ></canvas> */}
+      {children}
+    </Box>
+  );
 }
 
 export default MainPanel;
--- web-frontend/src/components/ProgressChart.jsx
+++ web-frontend/src/components/ProgressChart.jsx
@@ -4,104 +4,105 @@ import Chart from "react-apexcharts";
 import { Box, Text } from "@chakra-ui/react";
 
 class ProgressChart extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			data: props.data || 0,
-			label: props.label || "Progress",
-			color: props.color || "#87D4F9",
-			bg:
-				props.bg ||
-				"linear-gradient(90deg, rgba(6,53,58,1) 0%, rgba(7,28,52,1) 100%)",
-			textColor: props.textColor || "white",
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: props.data || 0,
+      label: props.label || "Progress",
+      color: props.color || "#87D4F9",
+      bg:
+        props.bg ||
+        "linear-gradient(90deg, rgba(6,53,58,1) 0%, rgba(7,28,52,1) 100%)",
+      textColor: props.textColor || "white",
+    };
+  }
 
-	render() {
-		const chartOptions = {
-			chart: {
-				height: 250,
-				type: "radialBar",
-				toolbar: {
-					show: false,
-				},
-			},
-			plotOptions: {
-				radialBar: {
-					hollow: {
-						margin: 0,
-						size: "70%",
-					},
-					track: {
-						background: "#313860",
-						strokeWidth: "100%",
-						margin: 0,
-					},
-					dataLabels: {
-						show: true,
-						name: {
-							show: false,
-						},
-						value: {
-							offsetY: 10,
-							color: this.state.textColor,
-							fontSize: "24px",
-							show: true,
-							formatter: function (val) {
-								return val + "%";
-							},
-						},
-					},
-				},
-			},
-			fill: {
-				type: "gradient",
-				gradient: {
-					shade: "dark",
-					type: "horizontal",
-					shadeIntensity: 0.5,
-					gradientToColors: [this.state.color],
-					stops: [0, 100],
-				},
-			},
-			stroke: {
-				lineCap: "round",
-			},
-		};
+  render() {
+    const chartOptions = {
+      chart: {
+        height: 360,
+        type: "radialBar",
+        toolbar: {
+          show: false,
+        },
+      },
+      plotOptions: {
+        radialBar: {
+          hollow: {
+            margin: 0,
+            size: "70%",
+          },
+          track: {
+            background: "#313860",
+            strokeWidth: "100%",
+            margin: 0,
+          },
+          dataLabels: {
+            show: true,
+            name: {
+              show: false,
+            },
+            value: {
+              offsetY: 10,
+              color: this.state.textColor,
+              fontSize: "24px",
+              show: true,
+              formatter: function (val) {
+                return val + "%";
+              },
+            },
+          },
+        },
+      },
+      fill: {
+        type: "gradient",
+        gradient: {
+          shade: "dark",
+          type: "horizontal",
+          shadeIntensity: 0.5,
+          gradientToColors: [this.state.color],
+          stops: [0, 100],
+        },
+      },
+      stroke: {
+        lineCap: "round",
+      },
+    };
 
-		return (
-			<Card
-				py="1rem"
-				height={{ sm: "300px" }}
-				width="100%"
-				bg={this.state.bg}
-				position="relative"
-				display="flex"
-				flexDirection="column"
-				justifyContent="space-between"
-			>
-				<Box width="100%" textAlign="center" pt="10px">
-					<Text color={this.state.textColor} fontSize="24px" fontWeight="bold">
-						{this.state.label}
-					</Text>
-				</Box>
-				<Box
-					flex="1"
-					display="flex"
-					alignItems="center"
-					justifyContent="center"
-				>
-					<Chart
-						options={chartOptions}
-						series={[this.state.data]}
-						type="radialBar"
-						width="100%"
-						height="140%"
-					/>
-				</Box>
-			</Card>
-		);
-	}
+    return (
+      <Card
+        py="1rem"
+        height={{ sm: "270px" }}
+        width="100%"
+        bg={this.state.bg}
+        position="relative"
+        display="flex"
+        flexDirection="column"
+        justifyContent="space-between"
+      >
+        <Box width="100%" textAlign="center" pt="10px">
+          <Text color={this.state.textColor} fontSize="24px" fontWeight="bold">
+            {this.state.label}
+          </Text>
+        </Box>
+        <Box
+          flex="1"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          className="apexChartContainer"
+        >
+          <Chart
+            options={chartOptions}
+            series={[this.state.data]}
+            type="radialBar"
+            width="100%"
+            height="140%"
+          />
+        </Box>
+      </Card>
+    );
+  }
 }
 
 export default ProgressChart;
