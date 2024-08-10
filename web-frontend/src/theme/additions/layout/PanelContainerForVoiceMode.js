53d08e1981ecc4c52df839ed69de12a302af0908
WILLIAM CHAN <wc2184@nyu.edu>
2024-08-10 16:32:06 -0400

    fix sean padding

--- web-frontend/src/theme/additions/layout/PanelContainerForVoiceMode.js
+++ web-frontend/src/theme/additions/layout/PanelContainerForVoiceMode.js
@@ -1,6 +1,6 @@
 const PanelContainerForVoiceMode = {
   baseStyle: {
-    p: window.location.pathname.includes("voicemode") ? "" : "30px 15px",
+    // p: "30px 15px",
     // p: "30px 15px",
     minHeight: "calc(100vh - 123px)",
     paddingTop: "0px",
