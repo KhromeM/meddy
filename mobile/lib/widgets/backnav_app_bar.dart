c66779e46401f53e282fb981960f281b6ac1e467
YaBoiAli <alinawab9830@gmail.com>
2024-08-12 02:22:19 +0400

    rebased

---
+++ mobile/lib/widgets/backnav_app_bar.dart
@@ -0,0 +1,51 @@
+// appbar for navigating back to home page
+import 'package:meddymobile/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:meddymobile/pages/my_home_page.dart';
+
+class BacknavAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final double appBarHeight = 56.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      leading: InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const MyHomePage()),
+          );
+        },
+        child: Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Icon(
+                Icons.circle,
+                size: 60,
+                color: orangeAccent,
+                // Color.fromARGB(255, 202, 235, 69) theme green
+                //Color.fromARGB(255, 255, 181, 93) theme orange
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 14, top: 3),
+                child: FaIcon(
+                  FontAwesomeIcons.arrowLeft,
+                  size: 20,
+                  color: Colors.white,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(appBarHeight);
+}
